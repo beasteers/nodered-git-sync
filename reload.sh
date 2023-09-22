ec47c9183d9db99bf76e883f77c41ddfb876dc2c
Bea Steers <bea.steers@gmail.com>
2023-09-22 02:04:59 -0400

    add default values for url and username
    and fix weird underscore in name

--- reload.sh
+++ reload.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+NODERED_URL="${NODERED_URL:-localhost:1880}"
+NODERED_USERNAME="${NODERED_USERNAME:-admin}"
+
 # Ensure required variables are set
-if [ -z "${NODE_RED_URL}" ] || [ -z "${NODE_RED_USERNAME}" ] || [ -z "${NODE_RED_PASSWORD}" ]; then
-    echo "Error: Please set NODE_RED_URL, NODE_RED_USERNAME, and NODE_RED_PASSWORD environment variables."
+if [ -z "${NODERED_URL}" ] || [ -z "${NODERED_USERNAME}" ] || [ -z "${NODERED_PASSWORD}" ]; then
+    echo "Error: Please set NODERED_URL, NODERED_USERNAME, and NODERED_PASSWORD environment variables."
     exit 1
 fi
 
 # Log in and obtain an access token
 echo "Getting token..."
-response=$(curl -X POST "${NODE_RED_URL}/auth/token" \
-        -d "client_id=node-red-admin&grant_type=password&scope=read&username=${NODE_RED_USERNAME}&password=${NODE_RED_PASSWORD}")
+response=$(curl -X POST "${NODERED_URL}/auth/token" \
+        -d "client_id=node-red-admin&grant_type=password&scope=read&username=${NODERED_USERNAME}&password=${NODERED_PASSWORD}")
 
 # Check if the response contains an access token
 access_token=$(echo "${response}" | sed -n 's/.*"access_token":"\([^"]*\)".*/\1/p')
@@ -23,7 +26,7 @@ AUTH="Authorization: Bearer ${access_token}"
 
 # Make a POST request to reload flows
 echo "Reloading flows..."
-response=$(curl -X POST "${NODE_RED_URL}/flows" \
+response=$(curl -X POST "${NODERED_URL}/flows" \
         -d '{"flows": [{"type": "tab"}]}' \
         -H "${AUTH}" \
         -H 'content-type: application/json; charset=utf-8' \
@@ -39,4 +42,4 @@ fi
 
 # Make a POST request to revoke the token
 echo "Revoking token..."
-curl -X POST "${NODE_RED_URL}/auth/revoke" -H "${AUTH}"
+curl -X POST "${NODERED_URL}/auth/revoke" -H "${AUTH}"
