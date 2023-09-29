f6b4dc20b309325a5912fa9b4b34c52edad430f4
Bea Steers <bea.steers@gmail.com>
2023-09-29 16:36:58 -0400

    permissions fix

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,45 @@ FROM alpine
 
 RUN apk add --no-cache git curl jq openssh
 
-RUN mkdir /scripts && chmod 777 /scripts
-RUN mkdir /backup && chmod 777 /backup
-WORKDIR /scripts
+ENV HOME /tmp
+ENV CUID 65532
+ENV CGID 65532
 
-ADD known_hosts /root/.ssh/
-ADD config /root/.ssh/
+# # add default user
+RUN addgroup --gid "$CGID" git-sync
+# RUN echo "git-sync:x:$CUID:$CGID::$HOME:/sbin/nologin" >> /etc/passwd
+RUN chmod 0666 /etc/passwd
+
+# make folders widely readable
+RUN mkdir -p -m 777 /scripts
+RUN mkdir -p -m 777 /backup
+RUN mkdir -p -m 777 /git
+RUN mkdir -p -m 777 $HOME && chown $CUID:$CGID $HOME
+RUN mkdir -p -m 777 $HOME/.ssh
+
+WORKDIR /scripts
+USER $CUID:$CGID
+# add the ssh config somewhere else
+# we create a new one within the 
+ADD known_hosts /scripts/src.ssh/
+ADD config /scripts/src.ssh/
+
+# global git ignore
 ADD global.gitignore /etc/.gitignore
 RUN git config --global core.excludesfile /etc/.gitignore
+RUN git config --global --add safe.directory /git
+RUN chmod 0777 $HOME/.gitconfig
 
 ADD pull.sh /scripts
 ADD update_nodered.sh /scripts
 
-ENV GITSYNC_DEST=/git
+# RUN mkdir -p -m 02775 "$HOME" && chown -R $CUID:$CGID "$HOME"
+# RUN chmod 777 /scripts/.ssh/*
+
+ENV GITSYNC_DEST /git
 ENV GITSYNC_REF main
 ENV GITSYNC_PERIOD 300
-ENV GITSYNC_EXECHOOK_COMMAND update_nodered.sh
-RUN mkdir $GITSYNC_DEST && chmod 777 $GITSYNC_DEST
+ENV GITSYNC_EXECHOOK_COMMAND /scripts/update_nodered.sh
+# RUN mkdir -p $GITSYNC_DEST && chmod 777 $GITSYNC_DEST
 
 CMD ["/scripts/pull.sh"]
--- pull.sh
+++ pull.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# create user
+id -un || echo "git-sync:x:$(id -u):$CGID::$HOME:/sbin/nologin\n" >> /etc/passwd
+id
+
+# setup home directory with SSH credentials
+mkdir -p $HOME/.ssh
+cp -r /scripts/src.ssh/* $HOME/.ssh/
+
+
+
+# git config --global --add safe.directory /git
+
 if [ -z "$GITSYNC_REPO" ]; then
 echo "Please set GITSYNC_REPO=https://github.com/your-git-repository"
     exit 1
@@ -8,16 +20,36 @@ fi
 GITSYNC_BACKUP_DIR="${GITSYNC_BACKUP_DIR:-/backup}"
 GITSYNC_DEST=$(realpath "${GITSYNC_DEST:-/git}")
 
+echo "# ---------------------------------- config ---------------------------------- #"
+echo "Git Remote URL: $GITSYNC_REPO"
+echo "Git Destination Folder: $GITSYNC_DEST"
+echo "Pull period (seconds): $GITSYNC_PERIOD"
+echo "Exec command: $GITSYNC_EXECHOOK_COMMAND"
+
 # check - does the directory exist and is not empty? if yes, backup the files and delete
 # BUT!! if the git repo is from "$GITSYNC_REPO" then don't do anything
 if [ -d "$GITSYNC_DEST" ]; then
-    echo "$GITSYNC_DEST already exists"
-    if [ -d "$GITSYNC_DEST/.git" ] && [ "$(git -C "$GITSYNC_DEST" config --get remote.origin.url)" = "$GITSYNC_REPO" ]; then
-        echo "but it's cool because it's the right git remote url!"
+    echo "# ---------------------------------------------------------------------------- #"
+    echo "Git Destination \"$GITSYNC_DEST\" already exists:"
+    ls -lah $GITSYNC_DEST
+
+    REMOTE_URL="$([ -d "$GITSYNC_DEST/.git" ] && git -C "$GITSYNC_DEST" remote get-url origin)"
+    echo "---"
+    echo "Detected Remote URL: $REMOTE_URL"
+    echo "Desired Remote URL: $GITSYNC_REPO"
+    if [ "$REMOTE_URL" = "$GITSYNC_REPO" ]; then
+        echo "hmm Looks good to me!"
     else
+        # back up the files
+        echo "# ------------------------- backing up existing files ------------------------ #"
         bkp=$GITSYNC_BACKUP_DIR/$(date +"%Y%m%d-%H%M%S")
-        echo "I'm not really sure what's up with the files so I'm going to move them to $bkp"
-        cp -r "$GITSYNC_DEST" "$bkp" && find "$GITSYNC_DEST" -mindepth 1 -delete        
+        echo "I'm not really sure what's up with the files so I'm going to move them"
+        echo "Backup destination: $bkp"
+        cp -r "$GITSYNC_DEST" "$bkp" && find "$GITSYNC_DEST" -mindepth 1 -delete  
+        ls -lah "$bkp"
+        echo
+        echo "Git destination is now clean:"
+        ls -lah "$GITSYNC_DEST"
     fi
 fi
 
@@ -26,23 +58,31 @@ while true; do
 # 
 if [ ! -d "$GITSYNC_DEST/.git" ]; then
     # clone the git repository
+    echo "# ---------------------------------- Cloning --------------------------------- #"
     echo "Cloning $GITSYNC_REPO to $GITSYNC_DEST"
-    git clone  --branch $GITSYNC_REF --single-branch $GITSYNC_REPO $GITSYNC_DEST || exit 1
+    git clone --branch $GITSYNC_REF --single-branch $GITSYNC_REPO $GITSYNC_DEST || exit 1
+    echo "# ---------------------------------------------------------------------------- #"
 fi
 
 cd $GITSYNC_DEST
+echo "--"
+echo "Checking at $(date)"
 
 # check if there are local changes. do nothing to avoid merge conflicts.
 if git status -uno --porcelain | grep -q .; then
-    echo "Branch has changes!"
+    echo "We have local uncommitted changes!"
+    echo "Please commit your changes before we can pull. I don't want to cause any trouble here."
 else
-    if [ -n "$(git fetch origin; git rev-list HEAD..FETCH_HEAD)" ]; then
+    # git pull --dry-run --rebase
+    git fetch origin
+    if [ -n "$(git rev-list HEAD..origin/"$GITSYNC_REF")" ] && git pull --dry-run --rebase; then
         # git pull
         echo "Pulling from $GITSYNC_REPO"
         if git pull --rebase; then
-            echo "Changes!"
+            echo "# --------------------------------- Changes! --------------------------------- #"
             # run update script only when git pulls anything successfully
-            [ ! -z $GITSYNC_EXECHOOK_COMMAND ] && $GITSYNC_EXECHOOK_COMMAND
+            [ ! -z $GITSYNC_EXECHOOK_COMMAND ] && echo "Running $GITSYNC_EXECHOOK_COMMAND" && $GITSYNC_EXECHOOK_COMMAND
+            echo "# ---------------------------------------------------------------------------- #"
         fi
     else
         echo "No changes to pull"
@@ -54,3 +94,5 @@ echo "Sleeping for $GITSYNC_PERIOD seconds..."
 sleep $GITSYNC_PERIOD
 
 done
+
+# git merge-tree $(git merge-base "origin/$GITSYNC_REF" "$GITSYNC_REF") master "$GITSYNC_REF"
